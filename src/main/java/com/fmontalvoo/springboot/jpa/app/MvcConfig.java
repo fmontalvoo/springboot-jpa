344cce9f6fc632195aa6f9355c9728dd89578291
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-10 15:49:51 -0500

    Se crea metodo que retorna un archivo de imagen a traves de una peticion GET.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/main/java/com/fmontalvoo/springboot/jpa/app/MvcConfig.java
+++ src/main/java/com/fmontalvoo/springboot/jpa/app/MvcConfig.java
@@ -2,11 +2,11 @@ package com.fmontalvoo.springboot.jpa.app;
 
 import java.nio.file.Paths;
 
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
 	/**
