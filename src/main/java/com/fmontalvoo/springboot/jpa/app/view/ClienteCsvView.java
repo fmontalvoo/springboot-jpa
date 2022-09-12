44128bc7015195e949a7617c4ead048536f02ee5
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-12 17:45:28 -0500

    Se agrega funcion para exportar a csv.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ src/main/java/com/fmontalvoo/springboot/jpa/app/view/ClienteCsvView.java
@@ -0,0 +1,48 @@
+package com.fmontalvoo.springboot.jpa.app.view;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import com.fmontalvoo.springboot.jpa.app.entities.Cliente;
+
+@Component("list")
+public class ClienteCsvView extends AbstractView {
+
+	public ClienteCsvView() {
+		setContentType("text/csv");
+	}
+
+	@Override
+	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		response.setHeader("Content-Disposition", "attachment; filename=\"Clientes.csv\"");
+		response.setContentType(getContentType());
+
+		Page<Cliente> clientes = (Page<Cliente>) model.get("clientes");
+
+		ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+
+		String[] header = { "id", "nombre", "apellido", "email", "createdAt" };
+		beanWriter.writeHeader(header);
+
+		for (Cliente cliente : clientes) {
+			beanWriter.write(cliente, header);
+		}
+		beanWriter.close();
+	}
+
+	@Override
+	protected boolean generatesDownloadContent() {
+		return true;
+	}
+
+}
